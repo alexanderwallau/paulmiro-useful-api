f5aab6d45707737e1f982337cdec8aad741c120c
paulmiro <30203227+paulmiro@users.noreply.github.com>
2026-01-05 17:44:01 +0100

    init axum

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(handler));
+
+    // run it with hyper on localhost:3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> String {
+    "Hello, World!".to_string()
+}
